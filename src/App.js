45cee6b7cd9395df06c6523b195537a70ac4cbad
Matthew Kiss <matthewkiss@Matthews-MacBook-Air.local>
2022-02-27 18:24:34 +0000

    style form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Enquire from './components/enquire/Enquire'
 import PriceList from './components/PriceList';
 import Home from './components/home/Home'
+import Footer from './components/layout/Footer';
 
 function App() {
 
@@ -11,12 +12,13 @@ function App() {
     <Router>
       <div>
         <Header />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/enquire' element={<Enquire />} />
+          <Route exact path='/price_list' element={<PriceList />} />
+        </Routes>
+        <Footer />
       </div>
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/enquire' element={<Enquire />} />
-        <Route exact path='/price_list' element={<PriceList />} />
-      </Routes>
     </Router>
   );
 }
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,25 +2,25 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <section>
-            <div>
-                <h3>Flair</h3>
+        <section id="section-3" className='footer'>
+            <div className='footer-element'>
+                <h3 className='h'>Flair</h3>
                 <ul>
                     <li><a href='price_list'>Price List</a></li>
-                    <li><a href='enquire'>Enquire Now</a></li>
+                    <li><a href='enquire'>   Enquire Now</a></li>
                 </ul>
             </div>
-            <div>
-                <h3>Legal</h3>
+            <div className='footer-element'>
+                <h3 className='h'>Legal</h3>
                 <ul>
-                    <li>Terms and Conditons</li>
+                    <li className='listPadding'>Terms and Conditons</li>
                     <li>Privacy</li>
                 </ul>
             </div>
-            <div>
-                <h3>Help</h3>
-                <ul>
-                    <li>Contact</li>
+            <div className='footer-element'>
+                <h3 className='h'>Help</h3>
+                <ul >
+                    <li className='h'>Contact</li>
                 </ul>
             </div>
         </section>
