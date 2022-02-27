23e20fdf93d1be379afcb187ed00d2c2895fbb95
Matthew Kiss <matthewkiss@Matthews-MacBook-Air.local>
2022-02-27 15:43:33 +0000

    create footer foundations

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <section>
+            <div>
+                <h3>Flair</h3>
+                <ul>
+                    <li><a href='price_list'>Price List</a></li>
+                    <li><a href='enquire'>Enquire Now</a></li>
+                </ul>
+            </div>
+            <div>
+                <h3>Legal</h3>
+                <ul>
+                    <li>Terms and Conditons</li>
+                    <li>Privacy</li>
+                </ul>
+            </div>
+            <div>
+                <h3>Help</h3>
+                <ul>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </section>
+    )
+}
+
+export default Footer
