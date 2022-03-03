364d780970f9f02b87c348054ca28a4299881e98
Matthew Kiss <matthewkiss@Matthews-MacBook-Air.local>
2022-03-03 15:30:43 +0000

    format footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,22 +3,22 @@ import React from 'react'
 const Footer = () => {
     return (
         <section id="cyanSection" className='footer'>
-            <div>
-                <h3 className='flair'>Flair</h3>
+            <div className='footer-item'>
+                <h3 >Flair</h3>
                 <ul className='footerList'>
                     <li>Price List</li>
                     <li>Enquire Now</li>
                 </ul>
             </div>
-            <div>
-                <h3 className='legal'>Legal</h3>
+            <div className='footer-item'>
+                <h3>Legal</h3>
                 <ul className='footerList'>
                     <li>Terms and Conditons</li>
                     <li>Privacy</li>
                 </ul>
             </div>
-            <div>
-                <h3 className='help'>Help</h3>
+            <div className='footer-item '>
+                <h3>Help</h3>
                 <ul className='footerList'>
                     <li>Contact</li>
                 </ul>
